9ffeff91848bfc74a84608b6b2b32a628ed1bf48
ZhuravlevAlexei <110524860+ZhuravlevAlexei@users.noreply.github.com>
2023-01-11 14:03:24 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
+  // console.log(' ' + position + ' ' + delay);
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(position, delay);
+      } else {
+        // Reject
+        reject(position, delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = evt.currentTarget;
+
+  for (let idx = 0; idx < amount.value; idx += 1) {
+    setTimeout(() => {
+      let curDelay = Number(delay.value) + Number(step.value * idx);
+      // console.log(curDelay);
+      createPromise(idx, curDelay);
+    }, step.value);
   }
 }
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmit);
+// console.log(form);
